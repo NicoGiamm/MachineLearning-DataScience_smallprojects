09bc8a9f50cc76667f989b1c5fe24e72b72dc1a5
NicoGiamm <nicolo.giammaria@gmail.com>
2023-03-19 17:08:39 +0100

    Added method for classifiers

--- Stock Forecast 2.0/ForecastSimulator2.py
+++ Stock Forecast 2.0/ForecastSimulator2.py
@@ -51,6 +51,46 @@ def forecast_split(data, window_size=30, dtp=1, test_days=30):
 
 ###############################################################################
 
+def forecast_split_classifier(data, window_size=30, dtp=1, test_days=30):
+    """
+    attributes:
+        data = time series data
+        window_size = time window to reshape data, after the reshape the data will have dim (data.shape[0] - window_size, window_size)
+        dtp = days to predict, it means how many days you want to predict for each row of X
+        test_days = number of rows of produced X_test
+    
+    how it works:
+        reshape data with n columns with n = window_size and y in m columns with m = dtp
+        split data in train and test
+        return splitted reshaped data
+    """
+    X = []
+    y_true = []
+    y_val = []
+    
+    for i in range(window_size, data.shape[0]-1-dtp):
+        X.append(data.values[i+1-window_size:i+1,0].tolist())
+        target = (data.values[i+1:i+1+dtp,0] - data.values[i:i+dtp,0])
+        if target >= 0:
+            y_true.append([1])
+        else:
+            y_true.append([0])
+            
+        y_val.append(data.values[i+1:i+1+dtp,0])
+        
+    X = np.asarray(X).astype('float32')
+    y_true = np.asarray(y_true).astype('float32')
+        
+    X_train = X[:-test_days,:]
+    X_test = X[-test_days:,:]
+    y_train = y_true[:-test_days]
+    y_test = y_true[-test_days:]
+    y_val = y_val[-test_days:]
+        
+    return X_train, X_test, y_train, y_test, y_val
+
+###############################################################################
+
 class Forecastsimulator():
     
     def __init__(self, model):
@@ -169,6 +209,38 @@ class Forecastsimulator():
         return prediction
     
     ###############################################################################
+    
+    def retrain_classifier(self, model, X_train, X_test, y_train, y_test, retrain_period=30):
+        
+        ''' 
+        retrain the given model of sklearn every given period
+        returns the prediction 
+        '''
+        
+        self.model = model
+        self.train(X_train, y_train)
+        prediction = np.empty(shape=(1,len(y_test)))
+        dim = X_test.shape[0]
+        
+        for i in range(dim):
+            
+            print(i)
+            pred = self.predict(X_test[0:1])
+            prediction = np.append(prediction, pred.reshape(-1,len(y_test)), axis = 0)
+            
+            X_train = np.append(X_train[1:], X_test[0:1], axis=0)
+            X_test = X_test[1:]
+            y_train = np.append(y_train[1:], y_test[0:1], axis=0)
+            y_test = y_test[1:]
+            
+            if ((i+1)%retrain_period == 0):
+                self.model = model
+                self.train(X_train, y_train)
+        
+        prediction = prediction[1:]
+        return prediction
+    
+    ###############################################################################
             
     def simulate(self, prediction, y_test):
         
@@ -200,6 +272,72 @@ class Forecastsimulator():
         for i in range(1, y_test.shape[0]-1):
             
             percentage = (abs(y_test[i,0] - y_test[i-1,0])/y_test[i-1,0])
+            
+            if ((pred_diff[i-1] <= 0 and true_diff[i-1] <= 0) or (pred_diff[i-1] > 0 and true_diff[i-1] > 0)):
+                score.append(1)
+                total += start*percentage
+                perc.append(percentage*100)
+            else:
+                score.append(0)
+                total -= start*percentage
+                perc.append(-percentage*100)
+                
+            tot.append(total)
+        
+        tot = np.array(tot)
+        sns.histplot(score, discrete=True)
+        score = np.array(score)
+        plt.title('Ratio of correct prediction up/down')
+        print(np.count_nonzero(score)/score.shape[0])
+        plt.show()
+        
+        perc = np.array(perc)
+        print(perc.mean(), perc.std())
+        plt.title('Distribution of returns')
+        plt.xlabel('Daily percentage of return')
+        sns.histplot(perc, kde = True)
+        plt.show () 
+        
+        print(total)
+        sns.lineplot(tot)
+        plt.xlabel('Days')
+        plt.ylabel('Balance')
+        plt.title('Test Balance')
+        plt.show()
+        
+        return  
+
+    ###################################################################################################
+    
+    def simulate_classifier(self, prediction, y_test, y_val):
+        
+        '''
+        evaluate a trading strategy starting from the prediction of a model and a real stock data
+        returns nothing, just fancy graph with some statistics and gains (or losses)
+        '''
+        
+        pred_diff = prediction.tolist()
+        true_diff = []
+
+        for i in range(y_test.shape[0]-1):
+            true_diff.append(y_val[i+1,0] - y_val[i,0])
+
+        sns.lineplot( pred_diff, label = 'predicted')
+        sns.lineplot( true_diff, label = 'true')
+        plt.xlabel('Time')
+        plt.ylabel('Price derivative')
+        plt.title('Test')
+        plt.show()
+        
+        score = []
+        start = 500
+        total = 0
+        tot = []
+        perc = []
+
+        for i in range(1, y_val.shape[0]-1):
+            
+            percentage = (abs(y_val[i,0] - y_val[i-1,0])/y_val[i-1,0])
             
             if ((pred_diff[i-1] < 0 and true_diff[i-1] < 0) or (pred_diff[i-1] >= 0 and true_diff[i-1] >= 0)):
                 score.append(1)
@@ -233,6 +371,6 @@ class Forecastsimulator():
         plt.title('Test Balance')
         plt.show()
         
-        return             
+        return                
         
             
